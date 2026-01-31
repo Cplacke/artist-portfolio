8b77a6d6fb674b43bded0de6a0e3cbf8a3170728
Kronos <kronos@kronoss-air.lan>
2026-01-31 14:20:34 -0500

    no file write on pod

--- util.js
+++ util.js
@@ -38,8 +38,8 @@ const saveFilesToImageDir = async (url, fileName) => {
 // saveFilesToImageDir(
 //     'https://media.canva.com/v2/image-resize/format:JPG/height:800/quality:92/uri:ifs%3A%2F%2FM%2F7a31c6d7-64ad-4637-8278-9215e7525afc/watermark:F/width:600?csig=AAAAAAAAAAAAAAAAAAAAALhr7JYS4VYb7u_5PlsxxnDijTSf52SnyEGzCaF4gcpY&exp=1769903044&osig=AAAAAAAAAAAAAAAAAAAAAIXkvmzCnZtAUROFxU4VWTV0RlrjBkx-EDG8-4io-oHh&signer=media-rpc&x-canva-quality=screen',
 //     'photo.jpg'
+// // );
+// saveFilesToImageDir(
+//     'https://media.canva.com/v2/image-resize/format:PNG/height:621/quality:100/uri:ifs%3A%2F%2FM%2Fb21bbbe4-9a01-4e35-a0bf-b83f188015ed/watermark:F/width:800?csig=AAAAAAAAAAAAAAAAAAAAAEtarnDcxQyTVUZbmmyA1kaj8GsGOYWXJ7nvRBV5Nz8N&exp=1769901897&osig=AAAAAAAAAAAAAAAAAAAAAItXoI9AkRI-7GtuTOCBJeGQgECHmY9NXI7Vv3ftn6OZ&signer=media-rpc&x-canva-quality=screen',
+//     'logo.png'
 // );
-saveFilesToImageDir(
-    'https://media.canva.com/v2/image-resize/format:PNG/height:621/quality:100/uri:ifs%3A%2F%2FM%2Fb21bbbe4-9a01-4e35-a0bf-b83f188015ed/watermark:F/width:800?csig=AAAAAAAAAAAAAAAAAAAAAEtarnDcxQyTVUZbmmyA1kaj8GsGOYWXJ7nvRBV5Nz8N&exp=1769901897&osig=AAAAAAAAAAAAAAAAAAAAAItXoI9AkRI-7GtuTOCBJeGQgECHmY9NXI7Vv3ftn6OZ&signer=media-rpc&x-canva-quality=screen',
-    'logo.png'
-);
